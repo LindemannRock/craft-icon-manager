0f41315c27a591cfcc620cfb2d1e1975e7a6c954
bhlindemann <bh@lindemannrock.com>
2026-05-20 22:45:50 +0400

    feat: add new properties to SettingsRecord for enhanced configuration

--- src/records/SettingsRecord.php
+++ src/records/SettingsRecord.php
@@ -14,8 +14,57 @@ use craft\db\ActiveRecord;
  * Settings Record
  *
  * @property int $id
- * @property string $name
- * @property string|null $value
+ * @property string|null $pluginName
+ * @property string $iconSetsPath
+ * @property bool $enableCache
+ * @property int $cacheDuration
+ * @property string $cacheStorageMethod
+ * @property string|null $enabledIconTypes
+ * @property bool $enableOptimization
+ * @property bool $enableOptimizationBackup
+ * @property bool $scanClipPaths
+ * @property bool $scanMasks
+ * @property bool $scanFilters
+ * @property bool $scanComments
+ * @property bool $scanInlineStyles
+ * @property bool $scanLargeFiles
+ * @property bool $scanWidthHeight
+ * @property bool $scanWidthHeightWithViewBox
+ * @property bool $optimizeAllowRiskyRules
+ * @property bool $optimizeConvertColorsToHex
+ * @property bool $optimizeConvertCssClasses
+ * @property bool $optimizeConvertEmptyTags
+ * @property bool $optimizeConvertInlineStyles
+ * @property bool $optimizeFixAttributeNames
+ * @property bool $optimizeFlattenGroups
+ * @property bool $optimizeMinifyCoordinates
+ * @property bool $optimizeMinifyTransformations
+ * @property bool $optimizeRemoveAriaAndRole
+ * @property bool $optimizeRemoveComments
+ * @property bool $optimizeRemoveDataAttributes
+ * @property bool $optimizeRemoveDefaultAttributes
+ * @property bool $optimizeRemoveDeprecatedAttributes
+ * @property bool $optimizeRemoveDoctype
+ * @property bool $optimizeRemoveDuplicateElements
+ * @property bool $optimizeRemoveEmptyGroups
+ * @property bool $optimizeRemoveEnableBackground
+ * @property bool $optimizeRemoveEmptyAttributes
+ * @property bool $optimizeRemoveEmptyTextElements
+ * @property bool $optimizeRemoveInkscapeFootprints
+ * @property bool $optimizeRemoveInvisibleCharacters
+ * @property bool $optimizeRemoveMetadata
+ * @property bool $optimizeRemoveNonStandardAttributes
+ * @property bool $optimizeRemoveNonStandardTags
+ * @property bool $optimizeRemoveTitleAndDesc
+ * @property bool $optimizeRemoveUnsafeElements
+ * @property bool $optimizeRemoveWhitespace
+ * @property bool $optimizeRemoveUnusedNamespaces
+ * @property bool $optimizeRemoveUnusedMasks
+ * @property bool $optimizeRemoveWidthHeight
+ * @property bool $optimizeScopeSvgStyles
+ * @property bool $optimizeSortAttributes
+ * @property string $logLevel
+ * @property int $itemsPerPage
  * @property \DateTime $dateCreated
  * @property \DateTime $dateUpdated
  * @property string $uid
